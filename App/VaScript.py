9e7ad0e813434fd852fb6bed478076f2745941c3
vrakitine <valeri.rakitine@gmail.com>
2023-02-11 17:55:24 -0500

    Added import psycopg2

--- App/VaScript.py
+++ App/VaScript.py
@@ -9,81 +9,41 @@ def getVaScript():
           "_action_description":{
               "_010":"--> init action"
           },
-            "Direction_10":"Action_010",  "_010":"/start",
-            "Direction_20":"Action_010",  "_020":"any text",
-            "Direction_30":"Action_010",  "_030":"answer yes_no",
-            "Direction_40":"Action_010",  "_040":"answer yes_no_uptoyou",
-            "Direction_50":"Action_010",  "_040":"again"
-      },
-      "Action_010":{
+            "Direction_10":"Action_010",  "_010":"/start"
+      },      
+      "Action_010_v":{
           "_agent_position":{
-              "en-US":"The v-agent is in /start",
-              "ru-RU":"v-agent в /start"
+              "en-US":"Welcome screen with instructions",
+              "ru-RU":"Начальный экран с приветствием и инструкцией"
           },
           "_action_description":{
-              "_010":"empty"
+              "_010":"Начальный экран с приветствием и инструкцией",
+              "_020":"dfdfdfdfdfdfdfd",
+              "_02220":"dfdfdfdfdfdf   dfdfdfdfd"
+
           },
-            "Direction_10":"Action_010",  "_010":"/start",
-            "Direction_20":"Action_010",  "_020":"any text",
-            "Direction_30":"Action_030",  "_030":"answer yes_no",
-            "Direction_40":"Action_040",  "_040":"answer yes_no_uptoyou",
-            "Direction_50":"Action_010",  "_040":"again"
-      },
-      "Action_020":{
-        "_agent_position":{
-              "en-US":"The v-agent is any text",
-              "ru-RU":"v-agent в any text"
-          },
-          "_action_description":{
-              "_010":"empty"
-          },
-        "Direction_10":"Action_010",  "_010":"/start",
-        "Direction_20":"Action_020",  "_020":"any text",
-        "Direction_30":"Action_030",  "_030":"answer yes_no",
-        "Direction_40":"Action_040",  "_040":"answer yes_no_uptoyou",
-        "Direction_50":"Action_010",  "_040":"again"
-      },
-      "Action_030":{
-        "_agent_position":{
-              "en-US":"The v-agent is answer yes_no",
-              "ru-RU":"v-agent в answer"
-          },
-          "_action_description":{
-              "_010":"empty"
-          },
-        "Direction_10":"Action_010",  "_010":"/start",
-        "Direction_20":"Action_010",  "_020":"any text",
-        "Direction_30":"Action_010",  "_030":"answer yes_no",
-        "Direction_40":"Action_010",  "_040":"answer yes_no_uptoyou",
-        "Direction_50":"Action_010",  "_040":"again"
-      },
-      "Action_040":{
-        "_agent_position":{
-              "en-US":"The v-agent is answer yes_no_uptoyou",
-              "ru-RU":"v-agent в answer"
-          },
-          "_action_description":{
-              "_010":"empty"
-          },
-        "Direction_10":"Action_010",  "_010":"/start",
-        "Direction_20":"Action_010",  "_020":"any text",
-        "Direction_30":"Action_010",  "_030":"answer yes_no",
-        "Direction_40":"Action_010",  "_040":"answer yes_no_uptoyou",
-        "Direction_50":"Action_010",  "_040":"again"
-      },   
-      "Action_9000":{
+            "Direction_10":"Action_020_v",  "_010":"Кнопка подписки",
+            "Direction_20":"Action_030_v",  "_010":"Кнопка Выставить счет"
+      },      
+      "Action_020_v":{
           "_agent_position":{
-              "en-US":"END",
+              "en-US":"Subcription",
+              "ru-RU":"Подписка"
+          },
+          "_action_description":{
+              "_010":"Subcription / Информация о текущей подписке"
+          },
+            "Direction_10":"Action_010_v",  "_010":"Кнопка 'Назад'",
+      },
+      "Action_030_v":{
+          "_agent_position":{
+              "en-US":"",
               "ru-RU":""
           },
           "_action_description":{
-              "_010":"empty"
+              "_010":""
           },
-          "Direction_10":"Action_END",  "_010":"",
-          "Direction_20":"Action_END",  "_020":"",
-          "Direction_30":"Action_30",  "_030":"answer" ,
-          "Direction_1000":"Action_END",  "_030":""
-      }
-    }
-
-    return va_script
+            "Direction_10":"Action_XXX",  "_010":"",
+            "Direction_20":"Action_XXX_v",  "_010":""
+      }      
+    };
--- App/main.py
+++ App/main.py
@@ -5,6 +5,8 @@ import sys
 fpath = os.path.join(os.path.dirname(__file__), 'VaActionsFolder')
 sys.path.append(fpath)
 
+import psycopg2
+
 import VaBox
 import VaScript
 import VaConfig
@@ -12,9 +14,44 @@ import VaConfigBot
 from VaData import VaData
 
 
-#print(sys.path)
 
-#exit(1)
+hostname = 'localhost'
+database = 'postgres'
+username = 'postgres'
+pwd = 'Postg!2408'
+port_id = 5432
+
+conn = None
+cur = None
+
+try:
+    conn = psycopg2.connect(
+            host = hostname,
+            dbname = database,
+            user = username,
+            password = pwd,
+            port = port_id
+        )
+    cur = conn.cursor()
+
+    insert_script = 'INSERT INTO payment (id, chat_id) VALUES (%s,%s)'
+    insert_value = (4,'test 4')
+
+    cur.execute(insert_script, insert_value)
+
+    conn.commit()
+except Exception as error:
+    print('error:[ ' + str(error) + ' ]')
+finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()  
+    print('\nThe end in -finally-')
+    exit(1)  
+
+print('\nThe end')
+exit(1)
 
 
 va_data = VaData()
