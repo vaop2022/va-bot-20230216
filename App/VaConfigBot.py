b2bdde3f95462c6e42af492f6adb4202c25125dc
vrakitine <valeri.rakitine@gmail.com>
2023-02-12 08:56:20 -0500

    Added val dev key to The Bot variables setting

--- App/VaConfigBot.py
+++ App/VaConfigBot.py
@@ -4,8 +4,12 @@ import telebot
 def setup(bot):
 
     ### The Bot variables setting
+    # production key
+    #bot.defineVariable('telebot.TeleBot(5662687046******)...b10', telebot.TeleBot('5631379194:AAH0hQ6mcX8h18cPD0WU8YRVC5fg2rWBGcM'))
+    
+    # val dev key
+    bot.defineVariable('telebot.TeleBot(5662687046******)...b10', telebot.TeleBot('6273699064:AAEw16IHpA_YEJ6a5nQdx-DkhpmojZzpYSQ'))
 
-    bot.defineVariable('telebot.TeleBot(5662687046******)...b10', telebot.TeleBot('5631379194:AAH0hQ6mcX8h18cPD0WU8YRVC5fg2rWBGcM'))
     bot.defineVariable('message from customer...b11', 'unknown message from customer')
     bot.defineVariable('message type from customer...message type', 'unknown message type from customer')
     bot.defineVariable('input [call.data] from customer...input from customer', 'unknown input [call.data] from customer')
