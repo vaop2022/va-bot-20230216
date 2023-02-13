6eb45b0b3cfccb9a39af0ad0dcb9a057e6929fde
vrakitine <valeri.rakitine@gmail.com>
2023-02-12 20:20:27 -0500

    Added authors = Table(

--- App/main_work_with_db_SQLAlchemy_example.py
+++ App/main_work_with_db_SQLAlchemy_example.py
@@ -14,8 +14,17 @@ import VaConfig
 import VaConfigBot
 from VaData import VaData
 
-from sqlalchemy import URL
-from sqlalchemy import create_engine
+from sqlalchemy import URL, create_engine, select, Table, Column, Integer, String, MetaData, ForeignKey
+
+meta = MetaData() 
+
+authors = Table(
+    'Authors', meta,
+    Column('id', Integer, primary_key = True),
+    Column('name', String(250), nullable = False)
+)
+
+print(authors.c)
 
 hostname = 'localhost'
 database = 'postgres'
@@ -31,8 +40,12 @@ url_object = URL.create(
     database=database,
 )
 
-engine = create_engine(url_object)
+engine = create_engine(url_object, echo = True)
+meta.create_all(engine)
 
+conn = engine.connect()
+ins_author_query = authors.insert().values(name = 'Val')
+conn.execute(ins_author_query)
 
 print('\nThe end')
 exit(1)
