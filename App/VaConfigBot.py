51f12cea89017c6f8bba7d6ed5b8f3872d446d93
Konstantin Rakitine <krakitine@pm.me>
2023-02-13 20:24:25 -0500

    Add .env.template and config usage

--- App/VaConfigBot.py
+++ App/VaConfigBot.py
@@ -1,11 +1,14 @@
+from dotenv import dotenv_values
 from VaScript import getVaScript
 import telebot
 
 def setup(bot):
 
-    ### The Bot variables setting
+    config = dotenv_values(".env")
 
-    bot.defineVariable('telebot.TeleBot(5662687046******)...b10', telebot.TeleBot('5631379194:AAH0hQ6mcX8h18cPD0WU8YRVC5fg2rWBGcM'))
+    ### The Bot variables setting
+    
+    bot.defineVariable('telebot.TeleBot(5662687046******)...b10', telebot.TeleBot(config.get("TELEGRAM_BOT_TOKEN")))
     bot.defineVariable('message from customer...b11', 'unknown message from customer')
     bot.defineVariable('message type from customer...message type', 'unknown message type from customer')
     bot.defineVariable('input [call.data] from customer...input from customer', 'unknown input [call.data] from customer')
