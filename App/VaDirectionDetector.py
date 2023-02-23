9722a20070fd62e6be529f39a9a3762043395f92
vaop2023 <vaop2023@gmail.com>
2023-02-22 19:44:12 -0500

    Uodated def getDirection

--- App/VaDirectionDetector.py
+++ App/VaDirectionDetector.py
@@ -2,7 +2,10 @@ def getDirection(va_data,bot_data):
     print('start dd')
 
     va_data.set('Direction...direction', 'The_code_of_the_direction_is _unknown')
+    message = bot_data.get('message from customer...b11')
 
+    if va_data.isContextExist(message.chat.id):
+        va_data.getContext(message.chat.id)
 
     
     def Action_000_case(va_data,bot_data):
@@ -20,18 +23,28 @@ def getDirection(va_data,bot_data):
     def Action_010_case(va_data,bot_data):
         pass
         print('Action_010_case', va_data)
+        if  bot_data.get('message type from customer...message type') == bot_data.get('message_type constant: input...input'):
+            if bot_data.get('input [call.data] from customer...input from customer') == bot_data.get('input from customer constant...yes or no'):
+                va_data.set('Direction...direction', 'Direction_30')
+            if bot_data.get('input [call.data] from customer...input from customer') == bot_data.get('input from customer constant...yes or no or uptoyou'):
+                va_data.set('Direction...direction', 'Direction_40')
+            #if bot_data.get('input [call.data] from customer...input from customer') == bot_data.get('input from customer constant...again'):
+            #    va_data.set('Direction...direction', 'Direction_50')
+    
 
+    
     def Action_020_case(va_data,bot_data):
         pass
         print('Action_020_case')
-
+    
     def Action_030_case(va_data,bot_data):
         pass
         print('Action_030_case')
-
+        va_data.set('Direction...direction', 'Direction_10')
     def Action_040_case(va_data,bot_data):
         pass
         print('Action_040_case')
+        va_data.set('Direction...direction', 'Direction_10')
 
     def Action_9000_case(va_data,bot_data):
         pass
