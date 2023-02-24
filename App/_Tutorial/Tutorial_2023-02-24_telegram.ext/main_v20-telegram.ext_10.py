e8db593beb7145a7908bf4fbdbcd720849ebf38b
vaop2023 <vaop2023@gmail.com>
2023-02-24 15:12:46 -0500

    Added caps_handler

--- App/_Tutorial/Tutorial_2023-02-24_telegram.ext/main_v20-telegram.ext_10.py
+++ App/_Tutorial/Tutorial_2023-02-24_telegram.ext/main_v20-telegram.ext_10.py
@@ -16,6 +16,9 @@ async def val(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
 
 
@@ -25,9 +28,11 @@ if __name__ == '__main__':
     start_handler = CommandHandler('start', start)
     val_handler = CommandHandler('val', val)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    caps_handler = CommandHandler('caps', caps)
 
     application.add_handler(start_handler)
     application.add_handler(val_handler)
     application.add_handler(echo_handler)
+    application.add_handler(caps_handler)
     
     application.run_polling()
