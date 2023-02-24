ca6d102dc754377c4c454f8f3aa9bd41db83a44a
vaop2023 <vaop2023@gmail.com>
2023-02-24 14:20:48 -0500

    Added val_handler

--- App/_Tutorial/Tutorial_2023-02-24_telegram.ext/main_v20-telegram.ext_10.py
+++ App/_Tutorial/Tutorial_2023-02-24_telegram.ext/main_v20-telegram.ext_10.py
@@ -1,1 +1,26 @@
 # from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Extensions-%E2%80%93-Your-first-Bot
+
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="/srart I'm a bot, please talk to me!")
+async def val(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="/val I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('6273699064:AAEw16IHpA_YEJ6a5nQdx-DkhpmojZzpYSQ').build()
+    
+    start_handler = CommandHandler('start', start)
+    val_handler = CommandHandler('val', val)
+    
+    application.add_handler(start_handler)
+    application.add_handler(val_handler)
+    
+    application.run_polling()
