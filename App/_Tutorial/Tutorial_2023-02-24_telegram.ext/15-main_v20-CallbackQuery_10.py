39d9a5a06320bd754dc86feb6e5c99181d9bd267
vaop2023 <vaop2023@gmail.com>
2023-02-24 17:01:06 -0500

    Added execute the same command three times

--- App/_Tutorial/Tutorial_2023-02-24_telegram.ext/15-main_v20-CallbackQuery_10.py
+++ App/_Tutorial/Tutorial_2023-02-24_telegram.ext/15-main_v20-CallbackQuery_10.py
@@ -1,4 +1,40 @@
 # from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Extensions-%E2%80%93-Your-first-Bot
+# and from https://youtu.be/StseQeseJbQ - CallbackQuery КОЛБЭКИ TELEGRAM БОТ -AIOGRAM (для чайников) - ПРОГРАММИРОВАНИЕ ПО ПЛЕЙЛИСТАМ УРОК 12
+
+
+"""
+How to write a bot telegram so that when you enter and execute the same command three times in a row, a message is displayed that you have already entered and executed this command three times in a row, for example, the command "/help"
+
+from telegram.ext import Updater, CommandHandler
+
+# Define a dictionary to keep track of command counts
+command_counts = {}
+
+# Define a function to handle the /help command
+def help(update, context):
+    # Get the user ID and command from the update
+    user_id = update.effective_user.id
+    command = "/help"
+
+    # Get the current count for this user and command
+    count = command_counts.get((user_id, command), 0)
+
+    if count == 2:
+        # If this is the third time the command has been executed in a row, send a warning message
+        context.bot.send_message(chat_id=update.message.chat_id, text="You have already executed this command three times in a row!")
+    else:
+        # Otherwise, increment the count and execute the command as normal
+        command_counts[(user_id, command)] = count + 1
+        context.bot.send_message(chat_id=update.message.chat_id, text="This is the first or second time you have executed this command in a row.")
+
+# Create an Updater object and register the command handler
+updater = Updater(token='6273699064:AAEw16IHpA_YEJ6a5nQdx-DkhpmojZzpYSQ', use_context=True)
+updater.dispatcher.add_handler(CommandHandler('help', help))
+
+# Start the bot
+updater.start_polling()
+
+"""
 
 import logging
 from telegram import Update
@@ -6,6 +42,8 @@ from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTyp
 from telegram import InlineQueryResultArticle, InputTextMessageContent
 from telegram.ext import InlineQueryHandler
 
+command_counts = {}
+count = 0
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -13,9 +51,26 @@ logging.basicConfig(
 )
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+    command = "/help"
+    command_counts[(user_id, command)] = 0
     await context.bot.send_message(chat_id=update.effective_chat.id, text="/srart I'm a bot, please talk to me!")
+
 async def val(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="/val I'm a bot, please talk to me!")
+    # Get the user ID and command from the update
+    user_id = update.effective_user.id
+    command = "/help"
+
+    # Get the current count for this user and command
+    count = command_counts.get((user_id, command), 0)
+
+    if count == 2:
+        # If this is the third time the command has been executed in a row, send a warning message
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="/val the third time the command has been executed!")
+    else:
+        # Otherwise, increment the count and execute the command as normal
+        command_counts[(user_id, command)] = count + 1
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="/val I'm a bot, please talk to me!")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
@@ -62,3 +117,4 @@ if __name__ == '__main__':
     application.add_handler(unknown_handler)
     
     application.run_polling()
+
