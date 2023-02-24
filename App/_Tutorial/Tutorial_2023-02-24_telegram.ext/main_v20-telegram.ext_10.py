eb543a2b78fef8f9e01fb2fa6fd2d08e706566be
vaop2023 <vaop2023@gmail.com>
2023-02-24 14:37:16 -0500

    Added echo_handler

--- App/_Tutorial/Tutorial_2023-02-24_telegram.ext/main_v20-telegram.ext_10.py
+++ App/_Tutorial/Tutorial_2023-02-24_telegram.ext/main_v20-telegram.ext_10.py
@@ -2,7 +2,7 @@
 
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -14,13 +14,20 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def val(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="/val I'm a bot, please talk to me!")
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token('6273699064:AAEw16IHpA_YEJ6a5nQdx-DkhpmojZzpYSQ').build()
     
     start_handler = CommandHandler('start', start)
     val_handler = CommandHandler('val', val)
-    
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+
     application.add_handler(start_handler)
     application.add_handler(val_handler)
+    application.add_handler(echo_handler)
     
     application.run_polling()
